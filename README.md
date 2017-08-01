05e341a81be98e308ceee92f881db204fd7e51d0
zzy <1097088392@qq.com>
2017-08-02 00:41:41 +0800

    init2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # vue-canvas
-基于CreateJs 的练习项目(CreateJS 是一套可以构建丰富交互体验的 HTML5 游戏的开源工具包)
+canvas的练习项目
---
+++ src/component/stage.vue
@@ -0,0 +1,47 @@
+<template>
+  <canvas ref="canvas" id="canvas" :width="size.width" :height="size.height">
+
+  </canvas>
+</template>
+
+<script>
+export default {
+  name: 'Stage',
+  props:{
+    size:{
+      type:Object,
+      default(){
+        return {
+          width:300,
+          height:150
+        }
+      }
+    }
+  },
+  data () {
+    return {
+     
+    }
+  },
+  computed:{
+    
+  },
+  created(){
+    
+  },
+  mounted(){
+    if(!this.stage){
+      this.stage = new createjs.Stage(this.$refs.canvas);
+      var image = new createjs.Bitmap("src/assets/logo.png");
+      this.stage.addChild(image);
+      setTimeout(()=>{
+        this.stage.update();
+      },1000) 
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
